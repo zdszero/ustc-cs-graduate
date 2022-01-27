34bf07197448f92e98b8b77e86d30ca943384690
zdszero <1335908147@qq.com>
2022-01-27 12:10:16 +0800

    fix bug in 2020-3 code

--- 机试历年真题/2020/3.cpp
+++ 机试历年真题/2020/3.cpp
@@ -4,21 +4,25 @@
 #include <vector>
 using namespace std;
 
+bool cmp(const string &l, const string &r) {
+  int lsize = l.size();
+  int rsize = r.size();
+  int maxv = max(l.size(), r.size());
+  for (int i = 0; i < maxv; i++) {
+    if (l[i % lsize] == r[i % rsize])
+      continue;
+    return l[i % lsize] > r[i % rsize];
+  }
+  return true;
+};
+
 int main(int argc, char *argv[]) {
   ifstream ifs("./number.in");
   vector<string> v;
   string s;
-  while (ifs >> s) v.push_back(s);
-  sort(v.begin(), v.end(), [](const string &l, const string &r) {
-    int minv = min(l.size(), r.size());
-    int maxv = max(l.size(), r.size());
-    for (int i = 0; i < maxv; i++) {
-      if (l[i % minv] == r[i % minv])
-        continue;
-      return l[i % minv] > r[i % minv];
-    }
-    return true;
-  });
-  for (auto &s : v) cout << s;
+  while (ifs >> s)
+    v.push_back(s);
+  sort(v.begin(), v.end(), cmp);
+  for (const auto &s : v) cout << s;
   return 0;
 }
