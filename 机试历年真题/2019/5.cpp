0fab13d70f86de3b5e500c39821cd2c3ef08ad71
zdszero <1335908147@qq.com>
2022-03-05 11:08:40 +0800

    fix bug in 2019-5

--- 机试历年真题/2019/5.cpp
+++ 机试历年真题/2019/5.cpp
@@ -1,54 +1,69 @@
 #include <fstream>
 #include <iostream>
+
 using namespace std;
 
-struct TreeNode {
-  TreeNode *lchild, *rchild;
-  int val;
-  TreeNode(int val) : lchild(nullptr), rchild(nullptr), val(val) {}
-};
+const int N = 100010;
 
-TreeNode *root;
+int tree[N], mx[N], mx2[N], mxr[N],
+    mx2r[N];  // longest, 2nd longest, next node of longest, next node of 2nd
+              // longest
 
-void insertNode(int val) {
-  if (!root) {
-    root = new TreeNode(val);
-    return;
-  }
-  TreeNode *t = root, *pre;
-  while (t) {
-    pre = t;
-    if (val >= t->val)
-      t = t->rchild;
-    else
-      t = t->lchild;
-  }
-  if (val >= pre->val)
-    pre->rchild = new TreeNode(val);
+int down(int u) {
+  if (!tree[u]) return 0;
+  int llen = down(u << 1);
+  int rlen = down((u << 1) + 1);
+
+  if (llen > rlen)
+    mxr[u] = u << 1, mx2r[u] = (u << 1) + 1;
   else
-    pre->lchild = new TreeNode(val);
+    mxr[u] = (u << 1) + 1, mx2r[u] = u << 1;
+
+  mx[u] = max(llen, rlen) + tree[u];
+  mx2[u] = min(llen, rlen) + tree[u];
+  return mx[u];
 }
 
-void dfs(TreeNode *t, int sum, int &max) {
-  if (!t) return;
-  if (!t->lchild && !t->rchild) {
-    if (sum + t->val > max) {
-      max = sum + t->val;
-      return;
+void up(int u, int p) {
+  if (!tree[u]) return;
+  if (p != -1) {
+    if (mxr[p] != u)  // parent's longest edge doesn't contain this node
+    {
+      if (mx[p] > mx[u]) {
+        mx2[u] = mx[u];
+        mx[u] = mx[p];
+        mx2r[u] = mxr[u];
+        mxr[u] = p;  // longest edge from parent
+      } else if (mx[p] > mx2[u]) {
+        mx2[u] = mx[p];
+        mx2r[u] = p;
+      }
+    } else {
+      if (mx2[p] > mx[u]) {
+        mx2[u] = mx[u];
+        mx[u] = mx2[p];
+        mx2r[u] = mxr[u];
+        mxr[u] = p;  // longest edge from parent
+      } else if (mx2[p] > mx2[u]) {
+        mx2[u] = mx2[p];
+        mx2r[u] = p;
+      }
     }
   }
-  if (t->rchild) dfs(t->rchild, sum + t->val, max);
-  if (t->lchild) dfs(t->lchild, sum + t->val, max);
+
+  up(u << 1, u);
+  up((u << 1) + 1, u);
 }
 
-int main(int argc, char *argv[]) {
-  ifstream ifs("./tree.in");
-  int num;
-  while (ifs >> num) insertNode(num);
-  int lhs_max = 0, rhs_max = 0;
-  dfs(root->lchild, 0, lhs_max);
-  dfs(root->rchild, 0, rhs_max);
-  int ans = root->val + lhs_max + rhs_max;
-  cout << ans;
-  return 0;
+int main() {
+  ifstream ifs("expr.in");
+  int i = 1;
+  for (; ifs >> tree[i]; i++)
+    ;
+  down(1);
+  up(1, -1);
+
+  int res = 0;
+  for (int j = 1; j <= i; ++j) res = max(res, mx[j] + mx2[j] - tree[j]);
+  cout << res;
 }
